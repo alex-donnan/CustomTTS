b472d0cc8ef4f6acf9e2cce4ccf4a8a9b38cb78f
Hannah-GBS <HannahRyanster@gmail.com>
2023-04-12 22:20:17 +0100

    Update requirements, gitignore, and add playsound logic & credential storage

--- ttsController.py
+++ ttsController.py
@@ -1,4 +1,5 @@
-from pydub import AudioSegment
+from num2words import num2words
+from playsound import playsound
 from TTS.api import TTS
 from twitchAPI.helper import first
 from twitchAPI.oauth import UserAuthenticator
@@ -7,71 +8,113 @@ from twitchAPI.twitch import Twitch
 from twitchAPI.types import AuthScope
 from uuid import UUID
 import configparser
+import emoji
+import json
 import os
 import queue
+import re
 import requests
 import urllib.parse
 
-class ttsController:
 
-    APP_ID = ''
-    APP_SECRET = ''
+class ttsController:
     USER_SCOPE = [AuthScope.BITS_READ]
-    PREFIXES = prefixes = ["Cheer", "hryCheer", "BibleThump", "cheerwhal", "Corgo", "uni", "ShowLove", "Party", "SeemsGood",
-                           "Pride", "Kappa", "FrankerZ", "HeyGuys", "DansGame", "EleGiggle", "TriHard", "Kreygasm", "4Head",
-                           "SwiftRage", "NotLikeThis", "FailFish", "VoHiYo", "PJSalt", "MrDestructoid", "bday", "RIPCheer",
+    PREFIXES = prefixes = ["Cheer", "hryCheer", "BibleThump", "cheerwhal", "Corgo", "uni", "ShowLove", "Party",
+                           "SeemsGood",
+                           "Pride", "Kappa", "FrankerZ", "HeyGuys", "DansGame", "EleGiggle", "TriHard", "Kreygasm",
+                           "4Head",
+                           "SwiftRage", "NotLikeThis", "FailFish", "VoHiYo", "PJSalt", "MrDestructoid", "bday",
+                           "RIPCheer",
                            "Shamrock"]
 
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
         self.output_path = os.path.join(self.config['DEFAULT']['OutputDirectory'], "output.wav")
+        self.brian_output_path = os.path.join(self.config['DEFAULT']['OutputDirectory'], "output.mp3")
+        self.credentials_path = os.path.join(self.config['DEFAULT']['OutputDirectory'], "credentials.json")
         self.target_channel = self.config['DEFAULT']['TargetChannel']
+        self.app_id = self.config['DEFAULT']['TwitchAppId']
+        self.app_secret = self.config['DEFAULT']['TwitchAppSecret']
         self.tts_queue = queue.Queue()
         self.tts_client = TTS(TTS.list_models()[0])
 
-    def remove_cheermotes(raw_message):
+    def remove_cheermotes(self, raw_message):
         word_list = raw_message.split(" ")
         message = ""
         for word in word_list:
-            if word.startswith(tuple(prefixes)) and word[-1].isdigit():
+            if word.startswith(tuple(ttsController.PREFIXES)) and word[-1].isdigit():
                 continue
             message += word + " "
         message.strip()
         return message
 
+    def convert_numbers(self, message):
+        message = re.sub('£(\\d+)', lambda m: num2words(m.group(1), to='currency', currency='GBP'), message)
+        message = re.sub('\\$(\\d+)', lambda m: num2words(m.group(1), to='currency', currency='USD'), message)
+        message = re.sub('(\\d+)', lambda m: num2words(m.group()), message)
+        return message
+
+    def replace_emoji(self, message):
+        message = re.sub(':[\\w_]+:', lambda m: re.sub('[:_]', ' ', m.group()), emoji.demojize(message))
+        return message
+
     def worker(self):
         while True:
-            if os.path.exists(self.output_path):
-                continue
             # if Cheer in queue, process it
             item = self.tts_queue.get()
-            message = remove_cheermotes(item['chat_message'])
+            message = self.remove_cheermotes(item['chat_message'])
             if item['bits_used'] == 2:
+                message = self.convert_numbers(message)
+                message = self.replace_emoji(message)
                 # do Coqui voice (just default voice atm)
-                self.tts_client.tts_to_file(text=message, file_path=self.output_path, speaker=self.tts_client.speakers[0],
-                                       language=self.tts_client.languages[0])
+                self.tts_client.tts_to_file(text=message, file_path=self.output_path,
+                                            speaker=self.tts_client.speakers[0],
+                                            language=self.tts_client.languages[0])
+                playsound(self.output_path)
+                os.remove(self.output_path)
                 self.tts_queue.task_done()
             else:
                 # Do brian
-                url = 'https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=' + urllib.parse.quote_plus(message)
+                url = 'https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=' + urllib.parse.quote_plus(
+                    message)
                 data = requests.get(url)
-                with open(os.path.join(self.config['DEFAULT']['OutputDirectory'], "output.mp3"), 'wb') as f:
+                with open(self.brian_output_path, 'wb') as f:
                     f.write(data.content)
                 f.close()
-                sound = AudioSegment.from_mp3(os.path.join(self.config['DEFAULT']['OutputDirectory'], "output.mp3"))
-                sound.export(self.output_path, format="wav")
-                os.remove(os.path.join(self.config['DEFAULT']['OutputDirectory'], "output.mp3"))
+                playsound(self.brian_output_path)
+                os.remove(self.brian_output_path)
+                self.tts_queue.task_done()
+
+    async def update_stored_creds(self, token, refresh):
+        with open(self.credentials_path, 'w') as f:
+            json.dump({'token': token, 'refresh': refresh}, f)
 
     async def on_cheer(self, uuid: UUID, data: dict) -> None:
         self.tts_queue.put(data)
 
     async def run(self):
-        twitch = await Twitch(ttsController.APP_ID, ttsController.APP_SECRET)
-        auth = UserAuthenticator(twitch, ttsController.USER_SCOPE, force_verify=False)
-        token, refresh_token = await auth.authenticate()
-        await twitch.set_user_authentication(token, ttsController.USER_SCOPE, refresh_token)
-        user = await first(twitch.get_users(logins=[self.target_channel]))
+        twitch = await Twitch(self.app_id, self.app_secret)
+        twitch.user_auth_refresh_callback = self.update_stored_creds
+        needs_auth = True
+        if os.path.exists(self.credentials_path):
+            with open(self.credentials_path) as f:
+                creds = json.load(f)
+            try:
+                await twitch.set_user_authentication(creds['token'], ttsController.USER_SCOPE, creds['refresh'])
+                user = await first(twitch.get_users(logins=[self.target_channel]))
+            except:
+                print('stored token invalid, refreshing...')
+            else:
+                needs_auth = False
+
+        if needs_auth:
+            auth = UserAuthenticator(twitch, ttsController.USER_SCOPE)
+            token, refresh_token = await auth.authenticate()
+            with open(self.credentials_path, 'w') as f:
+                json.dump({'token': token, 'refresh': refresh_token}, f)
+            await twitch.set_user_authentication(token, ttsController.USER_SCOPE, refresh_token)
+            user = await first(twitch.get_users(logins=[self.target_channel]))
 
         pubsub = PubSub(twitch)
         pubsub.start()
