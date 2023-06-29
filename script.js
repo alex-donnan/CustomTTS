47d32df4acff1e6f380d1ac709809f221ba4142a
Alex Donnan <39102022+alex-donnan@users.noreply.github.com>
2023-06-29 06:53:59 -0700

    Table fixes

--- script.js
+++ script.js
@@ -14,9 +14,14 @@ $(document).ready(() => {
   char_cnt.innerHTML = `Cheer Message: ${text_box.innerHTML.length} Characters`;
 
   slider.oninput = function() {
+    text = text.replace(/-{2,}/, '-');
+    text = text.replace(/\|{2,}/, '|');
+    text = text.replaceAll('|-|', '|');
+    text = text.replaceAll(' ', '');
+
     output.innerHTML = `BPM: ${this.value}`;
     text_box.innerHTML = `${base_text} ${this.value} ${text.substring(0, text.length - 1)}`;
-  char_cnt.innerHTML = `Cheer Message: ${text_box.innerHTML.length} Characters`;
+    char_cnt.innerHTML = `Cheer Message: ${text_box.innerHTML.length} Characters`;
   }
 
   const hot = new Handsontable(container, {
@@ -33,20 +38,23 @@ $(document).ready(() => {
   hot.addHook('afterChange', (changes) => {
     text = '';
     for (let x = 0; x < 500; x++) {
+      let row_text = '';
       for (let y = 0; y < 200; y++) {
         let el = hot.getDataAtCell(x, y);
         if (el != null) {
           text += `${el}-`;
+          row_text += `-`;
         }
       }
-      if (x != 499) text += '|';
+      if (x < 499 && row_text != '') text += '|';
     }
+
     text = text.replace(/-{2,}/, '-');
-    text = text.replace(/\|{2,}/, '|');
     text = text.replaceAll('|-|', '|');
+    text = text.replace(/\|{2,}/, '|');
     text = text.replaceAll(' ', '');
     
     text_box.innerHTML = `${base_text} ${slider.value} ${text.substring(0, text.length - 1)}`;
-  char_cnt.innerHTML = `Cheer Message: ${text_box.innerHTML.length} Characters`;
+    char_cnt.innerHTML = `Cheer Message: ${text_box.innerHTML.length} Characters`;
   });
 });
